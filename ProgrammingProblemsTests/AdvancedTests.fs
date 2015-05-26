45ac67566bf887697a16b2a9578b7f28c31aeb12
Andy Collins <askingalot@gmail.com>
2015-05-25 22:12:01 -0500

    part way to advanced problem 1

---
+++ ProgrammingProblemsTests/AdvancedTests.fs
@@ -0,0 +1,21 @@
+﻿module AdvancedTests
+
+open FsUnit
+open NUnit.Framework
+open NUnitRunner
+
+[<Test>]
+let ``Prob 1 - single letter``() =
+    Advanced.prob1_longestSubstring "a" "ab"
+    |> should equal "a"
+
+[<Test>]
+let ``Prob 1 - double letter``() =
+    Advanced.prob1_longestSubstring "ab" "aba"
+    |> should equal "ab"
+
+[<Test>]
+let ``Prob 1 - multiple substrings``() =
+    let result = Advanced.prob1_longestSubstring "abcabbaabb" "abcbbb"
+    result 
+    |> should equal "abc"
